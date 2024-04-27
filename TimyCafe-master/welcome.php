c71617695c390151dd1d0c3dce9ac4b799d0e91e
Your Name <marwa.bounoua@etu.uae.ac.ma>
2024-04-26 19:45:05 -0700

    added in a welcome php file

---
+++ TimyCafe-master/welcome.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<?php
+	if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+		echo "Name: " . $name . "<br />";
+		$email=$_POST['email'];
+		echo "Email: " . $email . "br />";
+}
+
+
+
+?>
+</body>
+</html>
